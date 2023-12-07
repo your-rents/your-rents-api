9ce99b102d734c8dd21219ded5e6fa737642c47e
Lucio Benfante <lucio@benfante.com>
2023-12-07 19:13:43 +0100

    Add initial Spring Boot application

---
+++ src/main/java/com/yourrents/api/YourRentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.yourrents.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourRentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourRentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourrents/api/YourRentsApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yourrents.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(TestYourRentsApiApplication.class)
+class YourRentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
