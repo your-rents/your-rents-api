98ac229954582128fb5f0ee11b17274af938fad9
Elena Canale <elena.canale@benfante.com>
2025-07-11 17:13:49 +0200

    add licence header

--- src/main/resources/db/migration/demo/R__init_demo_data.sql
+++ src/main/resources/db/migration/demo/R__init_demo_data.sql
@@ -1,3 +1,22 @@
+---
+-- #%L
+-- YourRents API
+-- %%
+-- Copyright (C) 2023 - 2025 Your Rents Team
+-- %%
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- #L%
+---
 DELETE FROM property;
 INSERT INTO property (id, name, address_id, year_of_build, type, description, size_mq, land_registry, external_id) VALUES (1000000,	'my flat',	NULL,	1971,	'Apartment',	'residential flat',	100,	'{"sub": "6", "classe": "02", "foglio": "AD/61", "country": "IT", "rendita": "51,13euro", "categoria": "Cat.C/6", "particella": "668", "ubicazione": "via De Gasperi, 54 - PT", "classamento": "", "consistenza": "15mq"}',	'00000000-0000-0000-0000-000000000001');
 INSERT INTO property (id, name, address_id, year_of_build, type, description, size_mq, land_registry, external_id) VALUES (1000001,	'my flat1',	NULL,	1971,	'Apartment',	'residential flat1',	100,	'{"sub": "6", "classe": "02", "foglio": "AD/61", "country": "IT", "rendita": "51,13euro", "categoria": "Cat.C/6", "particella": "668", "ubicazione": "via De Gasperi, 54 - PT", "classamento": "", "consistenza": "15mq"}',	'00000000-0000-0000-0000-000000000002');
